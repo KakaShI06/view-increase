5fb2e29591a5ec2c3499d53b25a8e4932e2a9409
ujjval.priyadarshi <ujjval.priyadarshi@lybrate.com>
2023-08-08 10:42:29 +0530

    Completing prototype

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,24 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   const [videoUrl, setVideoUrl] = useState<string>('')
   const [embeddedUrl, setEmbeddedUrl] = useState<string>('')
+  const [numberOfContainer, setNumberOfContainer] = useState(10)
 
   useEffect(() => {
+    if (videoUrl.includes('/shorts/')) {
+      const code = videoUrl.split('/shorts/')
+
+      if (code && code[1]) {
+        setEmbeddedUrl(`https://www.youtube.com/embed/${code[1]}?autoplay=1`)
+
+        return
+      }
+    }
+
     if (videoUrl) {
       const videoId = videoUrl.match(/v=([^&]+)/)
 
       if (videoId && videoId[1]) {
-        setEmbeddedUrl(`https://www.youtube.com/embed/${videoId[1]}`)
+        setEmbeddedUrl(`https://www.youtube.com/embed/${videoId[1]}?autoplay=1`)
       }
     }
   }, [videoUrl])
@@ -27,11 +38,22 @@ export default function Home() {
       <p className='text-xs text-center'>Paste your youtube video url</p>
 
       {embeddedUrl && (
-        <div>
-          <iframe
-            src={embeddedUrl}
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          ></iframe>
+        <div className='flex gap-2 flex-wrap pt-5 justify-around md:flex-col lg:flex-row'>
+          {Array(numberOfContainer)
+            .fill(setNumberOfContainer)
+            .map((_, index) => (
+              <div key={index} className="max-w-[45%]">
+                <iframe
+                  width='100%'
+                  height='579'
+                  src={embeddedUrl}
+                  title=''
+                  frameBorder='0'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                  allowFullScreen={false}
+                ></iframe>
+              </div>
+            ))}
         </div>
       )}
     </main>
